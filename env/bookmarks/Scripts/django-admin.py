f5b2a4cc13520e651b43ab0ad0805e9558d3ca36
Maciej Slawny <maciekslawny@gmail.com>
2021-08-08 17:08:36 +0200

    social wab app

---
+++ env/bookmarks/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maciek\desktop\git_maciekslawny\social-networking-website\env\bookmarks\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
